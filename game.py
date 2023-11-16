fd03e5de10c38959159eccb0a24906b46393e1f5
Hevanafa <dhevansafa4@hotmail.com>
2023-11-16 19:46:29 +0700

    added particle system

--- game.py
+++ game.py
@@ -6,7 +6,7 @@
 # script:  python
 
 # Comment this on release
-from typings import btn, cls, spr
+from typings import btn, circ, circb, cls, spr
 
 from math import atan2, pi as PI, sin, cos
 from random import random as rand, randint
@@ -17,6 +17,8 @@ def getDist(x1, x2, y1, y2):
 def getShootingAngle(dx: float, dy: float) -> float:
 	return atan2(dy, dx) + PI / 2
 
+def seconds2frames(sec: float):
+	return int(sec * 60)
 
 sw = 240
 sh = 136
@@ -30,14 +32,38 @@ px = 120.0
 py = 110.0
 capacity = 0
 
-class Garbage:
+class Vector:
 	def __init__(self):
 		self.cx = 0.0
 		self.cy = 0.0
 		self.vx = 0.0
 		self.vy = 0.0
 
+
+class Garbage(Vector):
+	pass
+	# def __init__(self):
+	# 	super().__init__()
+
+class Particle(Vector):
+	def __init__(self, colour: int):
+		super().__init__()
+		self.colour = colour
+		self.ttl = 0
+
 garbage_list: list[Garbage] = []
+
+particle_list: list[Particle] = []
+
+def emitParticles(x: int, y: int, colour: int):
+	for _ in range(1, 10):
+		p = Particle(colour)
+		p.cx = x
+		p.cy = y
+		p.vx = rand() - 0.5
+		p.vy = rand() - 0.5
+		p.ttl = seconds2frames(0.5 + rand())
+		particle_list.append(p)
 
 
 for a in range(1, 11):
@@ -72,11 +98,21 @@ def TIC():
 
 		if getDist(g.cx, px, g.cy, py) < 900:  # 30 pixels
 			rads = getShootingAngle(px - g.cx, py - g.cy)
-			g.vx = sin(rads)
-			g.vy = -cos(rads)
-		elif getDist(g.cx, px, g.cy, py) < 64:  # 8 pixels
+			g.vx = sin(rads) * 1.5
+			g.vy = -cos(rads) * 1.5
+
+		if getDist(g.cx, px, g.cy, py) < 64:  # 8 pixels
 			capacity += 1
+			emitParticles(g.cx, g.cy, 7)
 			garbage_list.remove(g)
+
+	for p in particle_list:
+		p.cx += p.vx
+		p.cy += p.vy
+		p.ttl -= 1
+
+		if p in particle_list and p.ttl <= 0:
+			particle_list.remove(p)
 
 
 	cls(0)
@@ -84,10 +120,14 @@ def TIC():
 	# garbage truck
 	spr(40, (sw - 16) // 2, sh - 17, 0, w=2, h=2)
 
-	# render all garbage
+	# garbage
 	for g in garbage_list:
 		spr(19, int(g.cx - 4), int(g.cy - 4), 0)
 
 	# player sprite
 	spr(36, int(px) - 8, int(py - 8), 0, w=2, h=2)
+
+	# particles
+	for p in particle_list:
+		circ(int(p.cx), int(p.cy), 1, 7)
 
